49dbef3ecc51be9e981a09279dc99c38a7561e8e
btozel <btozel@gmail.com>
2024-08-28 17:34:17 -0700

    initial commit

---
+++ FlappyBirdApp.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class FlappyBirdApp {
+
+    public static void main(String[] args) {
+        // Width of the game board
+        int boardWidth = 360;
+        // Height of the game board
+        int boardHeight = 640;
+        // Creating the JFrame for the game window with the title
+        JFrame gameWindow = new JFrame("Flappy Bird In Seattle");
+        // Set the size of the game window
+        gameWindow.setSize(boardWidth, boardHeight);
+        // Set the game window not resizable
+        gameWindow.setResizable(false);
+        // Center the game window to the screen
+        gameWindow.setLocationRelativeTo(null);
+        // Set closing operations
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        // Fixing the dimension problem, dimensions will start after the title bar
+        gameWindow.pack();
+
+        // Set game window visible
+        gameWindow.setVisible(true);
+    }
+}
